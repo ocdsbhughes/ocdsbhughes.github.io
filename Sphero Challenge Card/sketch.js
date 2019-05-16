17f38da88f08acae17959c3873815f66dd435719
ocdsbhughes <scott.hughes@ocdsb.ca>
2019-05-16 19:45:39 -0400

    Update sketch.js

--- Sphero Challenge Card/sketch.js
+++ Sphero Challenge Card/sketch.js
@@ -1,59 +1,42 @@
+//Assign Variables for the cover image and cards
 let cover;
-let possibleCards;
 let cardArray = [];
+
+//Check if a random card has been drawn
 let randCard = -1;
 
 function preload() {
+  //Load images
   cover = loadImage('images/AroundTheWorld_Cover.jpg')
-  possibleCards = [
-    './images/AroundTheWorld_Card_01.jpg',
-    './images/AroundTheWorld_Card_02.jpg',
-    './images/AroundTheWorld_Card_03.jpg',
-    './images/AroundTheWorld_Card_04.jpg',
-    './images/AroundTheWorld_Card_05.jpg',
-    './images/AroundTheWorld_Card_06.jpg',
-    './images/AroundTheWorld_Card_07.jpg',
-    './images/AroundTheWorld_Card_08.jpg',
-    './images/AroundTheWorld_Card_09.jpg',
-    './images/AroundTheWorld_Card_10.jpg',
-    './images/AroundTheWorld_Card_11.jpg',
-    './images/AroundTheWorld_Card_12.jpg',
-    './images/AroundTheWorld_Card_13.jpg',
-    './images/AroundTheWorld_Card_14.jpg',
-    './images/AroundTheWorld_Card_15.jpg',
-    './images/AroundTheWorld_Card_16.jpg',
-    './images/AroundTheWorld_Card_17.jpg',
-    './images/AroundTheWorld_Card_18.jpg',
-    './images/AroundTheWorld_Card_19.jpg',
-    './images/AroundTheWorld_Card_20.jpg',
-  ];
 
+  //Assign all cards to an array
   for (let i=0; i<20; i++) {
-    cardArray[i] = loadImage(possibleCards[i])
+    cardArray[i] = loadImage('./images/AroundTheWorld_Card_'+i+'.jpg')
     print('yeh!')
   }
-
-
-
-
 }
 
 function setup() {
+  //Create the canvas
   createCanvas(1275,1650)
 }
 
 function draw() {
   // put drawing code here
+  //Check if the random card variable was updated. If not, show the cover
   if (randCard===-1) {
     image(cover,0,0);
   }
+  //If random was updated, show the card (using array)
   else {
     background(255);
     image(cardArray[randCard],0,0);
   }
 }
 
+//Update the card on mouse press
 function mousePressed() {
   randCard = floor(random(possibleCards.length));
   print(randCard+"test")
 }
+*/
